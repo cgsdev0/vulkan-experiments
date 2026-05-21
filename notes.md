09c7fe20781d19d191f93505ba889eb104e0bab8
Sarah Schulte <git@sarah.engineer>
2026-05-21 18:33:26 -0400

    init

---
+++ notes.md
@@ -0,0 +1,5 @@
+references:
+- https://docs.vulkan.org/tutorial/latest/00_Introduction.html
+- https://vkguide.dev/
+- raytracing: https://github.com/RayTracing/raytracing.github.io
+- pbr book: https://www.pbr-book.org/
---
+++ shell.nix
@@ -0,0 +1,36 @@
+{ pkgs ? (import <nixpkgs> {}) }:
+with pkgs;
+mkShell {
+  buildInputs = [
+    # put packages here.
+    pkg-config
+    ninja
+    glslang # or shaderc
+    shaderc
+    shader-slang
+    vulkan-headers
+    vulkan-loader
+    vulkan-validation-layers # maybe?
+    glfw
+    libxkbcommon
+    wayland
+    tinyobjloader
+    tinygltf
+    glm
+    cmake
+    freetype
+    vulkan-tools        # vulkaninfo
+    renderdoc           # Graphics debugger
+    tracy               # Graphics profiler
+    vulkan-tools-lunarg # vkconfig
+    ktx-tools
+    stb
+  ];
+
+  shellHook = ''
+    export CMAKE_PREFIX_PATH=${pkgs.ktx-tools}:$CMAKE_PREFIX_PATH
+  '';
+
+  # If it doesn’t get picked up through nix magic
+  VULKAN_SDK = "${vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+}
